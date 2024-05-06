2b4d00f0b028bff9b70df65660a6e822a8482205
서주희 <maywngml@naver.com>
2024-05-06 17:56:05 +0900

    [Add] 1. 색상값 추가 2. 다크버전 색상값 라이트버전과 통일

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'beige-100': 'var(--beige-100)',
+        'beige-400': 'var(--beige-400)',
+        'purple-300': 'var(--purple-300)',
+        'purple-500': 'var(--purple-500)',
+        'purple-700': 'var(--purple-700)',
       },
     },
   },
