6f14c7671aed9b828b13230337a5c08e8dfc8f63
서주희 <maywngml@naver.com>
2024-08-13 14:29:33 +0900

    [Add] 날짜를 'yyyy.mm' 형태로 변환해주는 함수 추가

--- lib/utils/helpers.ts
+++ lib/utils/helpers.ts
@@ -11,3 +11,9 @@ export function preventBodyScroll(isPreventScroll: boolean) {
     bodyStyle.paddingRight = '0px';
   }
 }
+
+export function getFormattedDate(date: Date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  return `${year}.${month}`;
+}
