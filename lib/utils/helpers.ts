a8ef64fbef93ed7d254b7c5177a0d3023d330b9e
서주희 <maywngml@naver.com>
2024-08-17 14:07:56 +0900

    [Add] 이미지의 blurhash를 blurDataURL로 변경하는 함수 추가

--- lib/utils/helpers.ts
+++ lib/utils/helpers.ts
@@ -1,3 +1,24 @@
+import { decode } from 'blurhash';
+
+export function getBlurDataURL(blurhash: string) {
+  const WIDTH = 32;
+  const HEIGHT = 32;
+  const pixels = decode(blurhash, WIDTH, HEIGHT);
+  const canvas = document.createElement('canvas');
+
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  const context = canvas.getContext('2d');
+
+  if (!context) return undefined;
+
+  const imageData = context.createImageData(WIDTH, HEIGHT);
+  imageData.data.set(pixels);
+  context.putImageData(imageData, 0, 0);
+
+  return canvas.toDataURL('image/jpeg');
+}
+
 export function preventBodyScroll(isPreventScroll: boolean) {
   const scrollbarWidth =
     window.innerWidth - document.documentElement.clientWidth;
