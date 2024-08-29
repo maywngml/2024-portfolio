e83662a6f23e7f6bcccebfca74b70ee43f5d1a53
서주희 <maywngml@naver.com>
2024-08-29 21:29:49 +0900

    [Delete] 프로젝트 이미지 슬라이더 컴포넌트 제거

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 export { default as ImageWithPlaceholder } from './image-with-placeholder';
 export { default as Intro } from './intro';
 export { default as Introduction } from './introduction';
-export { default as ProjectImageSlider } from './project-image-slider';
 export { default as Project } from './project';
